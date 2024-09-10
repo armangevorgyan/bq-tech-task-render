3bbe80037431918792f7f0278b5be547ef74ac2c
arman.gevorgyan <armangev@gmail.com>
2024-09-11 00:56:36 +0400

    Add Notion links and initial styles to home page
    This commit adds two Notion links to the home page, including rendered and original Notion page links. Additionally, initial CSS styles for the page and main sections are added to enhance layout and alignment.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import './page.scss';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className={'page'}>
       <main className={'main'}>
-        <div>
-          <h1>Link to your final app / website.</h1>
-
-        </div>
-
+        <h1>Tech Task</h1>
+        <Link href={'notion'}>Rendered Notion</Link>
+        <Link href='https://bluequbit.notion.site/Sample-notion-page-575d3ec590204c938adc349bef9cabc9'><h3>Original Page Link</h3></Link>
       </main>
       <footer className={'footer'}>
       </footer>
